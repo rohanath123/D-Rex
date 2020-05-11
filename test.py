6ad2b221bdbd30265439cf1cab2daf5a2f1f307a
Rohan Athawade <39797571+rohanath123@users.noreply.github.com>
2020-05-11 20:41:20 +0530

    Generalized path, added note to make temp folder

--- test.py
+++ test.py
@@ -15,13 +15,13 @@ from ocr_test import *
 
 import cv2
 
-original_image = cv2.imread("D:/Deep Learning Training Data/Forms Dataset/images/images/5.jpg")
+original_image = cv2.imread("< PATH TO FORM IMAGE >")
 
 print("Pre-Processing Image for Prediction...")
 prediction_image = get_prediction_image(original_image)
 
 print("Initiating Trained Model...")
-model = get_pretrained_model('D:/Deep Learning Trained Models/Forms/100.pt')
+model = get_pretrained_model('< PATH TO 100.PT FILE >')
 model.eval()
 
 print("Making Prediction and Segmenting Image to get Boxes...")
@@ -29,6 +29,7 @@ boxes = custom_predict(model, pil(original_image), pil(prediction_image), False,
 
 print("Using OCR and writing Labels and Info...")
 for i in range(len(boxes)):
+	#CREATE A TEMPORARY FOLDER IN YOUR WORKING DIRECTORY CALLED "TEMP"
 	PATH = "./Temp/"+str(i)+".png"
 	label = get_cleaned_text(PATH, boxes[i], True)
 	info = get_cleaned_text(PATH, boxes[i], False)
@@ -38,10 +39,10 @@ for i in range(len(boxes)):
 	if info == ' ' or label == ' ':
 		continue
 	else:
-		with open("./Labels and Content/sunfeast_labels.txt", 'a', encoding="utf-8") as f:
+		with open("< PATH TO TXT FILE TO STORE LABELS >", 'a', encoding="utf-8") as f:
 			f.write(str(label))
 			f.write('\n')
 
-		with open("./Labels and Content/sunfeast_content.txt", 'a', encoding="utf-8") as f:
+		with open("< PATH TO TXT FILE TO STORE CONTENT >", 'a', encoding="utf-8") as f:
 			f.write(str(info))
 			f.write('\n')
