4a3311e1107e4e106627658e10bc70790280e09a
Rohan Athawade <39797571+rohanath123@users.noreply.github.com>
2020-05-11 20:31:45 +0530

    Clean code and added script to write to txt

--- test.py
+++ test.py
@@ -14,22 +14,34 @@ from helper import *
 from ocr_test import *
 
 import cv2
-#"D:/Deep Learning Training Data/Empty Forms/Empty Forms/13149651.png"
-#"C:/Users/Rohan/Desktop/Misc Images"
 
-original_image = cv2.imread("D:/Deep Learning Training Data/Empty Forms/Empty Forms/0030041455.png")
+original_image = cv2.imread("D:/Deep Learning Training Data/Forms Dataset/images/images/5.jpg")
 
+print("Pre-Processing Image for Prediction...")
 prediction_image = get_prediction_image(original_image)
 
+print("Initiating Trained Model...")
 model = get_pretrained_model('D:/Deep Learning Trained Models/Forms/100.pt')
 model.eval()
+
+print("Making Prediction and Segmenting Image to get Boxes...")
 boxes = custom_predict(model, pil(original_image), pil(prediction_image), False, True)
 
+print("Using OCR and writing Labels and Info...")
 for i in range(len(boxes)):
-	PATH = "C:/Users/Rohan/Desktop/Misc Images/"+str(i)+".png"
-	boxes[i].save(PATH)
-	text = detect_document(PATH)
-	if text == ' ':
+	PATH = "./Temp/"+str(i)+".png"
+	label = get_cleaned_text(PATH, boxes[i], True)
+	info = get_cleaned_text(PATH, boxes[i], False)
+
+	info = info.replace(label, '')
+
+	if info == ' ' or label == ' ':
 		continue
 	else:
-		print(text)
+		with open("./Labels and Content/sunfeast_labels.txt", 'a', encoding="utf-8") as f:
+			f.write(str(label))
+			f.write('\n')
+
+		with open("./Labels and Content/sunfeast_content.txt", 'a', encoding="utf-8") as f:
+			f.write(str(info))
+			f.write('\n')
